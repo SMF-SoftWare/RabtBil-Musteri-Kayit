ae488843a8212396c33efb1923cf98df8880b771
TempCSharp <36564655+TempCSharp@users.noreply.github.com>
2018-02-21 19:51:53 +0300

    Initial commit

---
+++ RabtBilMusteriKayit/RabtBilMusteriKayit/FrmGirisYap.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace RabtBilMusteriKayit
+{
+    public partial class FrmGirisYap : Form
+    {
+        public FrmGirisYap()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FrmTeknikServisFormu frm = new FrmTeknikServisFormu();
+            frm.Show();
+            this.Hide();
+        }
+    }
+}
---
+++ RabtBilMusteriKayit/RabtBilMusteriKayit/FrmTeknikServisFormu.Designer.cs
@@ -0,0 +1,386 @@
+﻿namespace RabtBilMusteriKayit
+{
+    partial class FrmTeknikServisFormu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.GrpBoxMusteriBilgileri = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.GrpBoxAksesuarlar = new System.Windows.Forms.GroupBox();
+            this.TxtAksesuarlar = new System.Windows.Forms.TextBox();
+            this.GrpBoxEkBilgiler = new System.Windows.Forms.GroupBox();
+            this.TxtEkBilgiler = new System.Windows.Forms.TextBox();
+            this.GrpBoxUrunBilgileri = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.GrpBoxMusteriBilgileri.SuspendLayout();
+            this.GrpBoxAksesuarlar.SuspendLayout();
+            this.GrpBoxEkBilgiler.SuspendLayout();
+            this.GrpBoxUrunBilgileri.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(4, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Müşteri Adı:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(96, 27);
+            this.textBox1.MaxLength = 30;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(252, 25);
+            this.textBox1.TabIndex = 1;
+            // 
+            // GrpBoxMusteriBilgileri
+            // 
+            this.GrpBoxMusteriBilgileri.Controls.Add(this.label3);
+            this.GrpBoxMusteriBilgileri.Controls.Add(this.textBox3);
+            this.GrpBoxMusteriBilgileri.Controls.Add(this.label2);
+            this.GrpBoxMusteriBilgileri.Controls.Add(this.textBox2);
+            this.GrpBoxMusteriBilgileri.Controls.Add(this.label1);
+            this.GrpBoxMusteriBilgileri.Controls.Add(this.textBox1);
+            this.GrpBoxMusteriBilgileri.Location = new System.Drawing.Point(12, 12);
+            this.GrpBoxMusteriBilgileri.Name = "GrpBoxMusteriBilgileri";
+            this.GrpBoxMusteriBilgileri.Size = new System.Drawing.Size(360, 131);
+            this.GrpBoxMusteriBilgileri.TabIndex = 3;
+            this.GrpBoxMusteriBilgileri.TabStop = false;
+            this.GrpBoxMusteriBilgileri.Text = "Müşteri Bilgileri";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(30, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Telefon:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(96, 89);
+            this.textBox3.MaxLength = 16;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(252, 25);
+            this.textBox3.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(23, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Kayıt No:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(96, 58);
+            this.textBox2.MaxLength = 30;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(252, 25);
+            this.textBox2.TabIndex = 3;
+            // 
+            // GrpBoxAksesuarlar
+            // 
+            this.GrpBoxAksesuarlar.Controls.Add(this.TxtAksesuarlar);
+            this.GrpBoxAksesuarlar.Location = new System.Drawing.Point(12, 149);
+            this.GrpBoxAksesuarlar.Name = "GrpBoxAksesuarlar";
+            this.GrpBoxAksesuarlar.Size = new System.Drawing.Size(360, 137);
+            this.GrpBoxAksesuarlar.TabIndex = 6;
+            this.GrpBoxAksesuarlar.TabStop = false;
+            this.GrpBoxAksesuarlar.Text = "Aksesuarlar";
+            // 
+            // TxtAksesuarlar
+            // 
+            this.TxtAksesuarlar.Location = new System.Drawing.Point(6, 21);
+            this.TxtAksesuarlar.MaxLength = 210;
+            this.TxtAksesuarlar.Multiline = true;
+            this.TxtAksesuarlar.Name = "TxtAksesuarlar";
+            this.TxtAksesuarlar.Size = new System.Drawing.Size(350, 110);
+            this.TxtAksesuarlar.TabIndex = 6;
+            // 
+            // GrpBoxEkBilgiler
+            // 
+            this.GrpBoxEkBilgiler.Controls.Add(this.TxtEkBilgiler);
+            this.GrpBoxEkBilgiler.Location = new System.Drawing.Point(12, 292);
+            this.GrpBoxEkBilgiler.Name = "GrpBoxEkBilgiler";
+            this.GrpBoxEkBilgiler.Size = new System.Drawing.Size(360, 137);
+            this.GrpBoxEkBilgiler.TabIndex = 7;
+            this.GrpBoxEkBilgiler.TabStop = false;
+            this.GrpBoxEkBilgiler.Text = "Ek Bilgiler";
+            // 
+            // TxtEkBilgiler
+            // 
+            this.TxtEkBilgiler.Location = new System.Drawing.Point(6, 21);
+            this.TxtEkBilgiler.MaxLength = 210;
+            this.TxtEkBilgiler.Multiline = true;
+            this.TxtEkBilgiler.Name = "TxtEkBilgiler";
+            this.TxtEkBilgiler.Size = new System.Drawing.Size(350, 110);
+            this.TxtEkBilgiler.TabIndex = 6;
+            // 
+            // GrpBoxUrunBilgileri
+            // 
+            this.GrpBoxUrunBilgileri.Controls.Add(this.label9);
+            this.GrpBoxUrunBilgileri.Controls.Add(this.textBox9);
+            this.GrpBoxUrunBilgileri.Controls.Add(this.pictureBox1);
+            this.GrpBoxUrunBilgileri.Controls.Add(this.label8);
+            this.GrpBoxUrunBilgileri.Controls.Add(this.label7);
+            this.GrpBoxUrunBilgileri.Controls.Add(this.textBox8);
+            this.GrpBoxUrunBilgileri.Controls.Add(this.label6);
+            this.GrpBoxUrunBilgileri.Controls.Add(this.textBox7);
+            this.GrpBoxUrunBilgileri.Controls.Add(this.label5);
+            this.GrpBoxUrunBilgileri.Controls.Add(this.textBox6);
+            this.GrpBoxUrunBilgileri.Controls.Add(this.comboBox1);
+            this.GrpBoxUrunBilgileri.Controls.Add(this.textBox5);
+            this.GrpBoxUrunBilgileri.Controls.Add(this.label4);
+            this.GrpBoxUrunBilgileri.Controls.Add(this.textBox4);
+            this.GrpBoxUrunBilgileri.Location = new System.Drawing.Point(378, 12);
+            this.GrpBoxUrunBilgileri.Name = "GrpBoxUrunBilgileri";
+            this.GrpBoxUrunBilgileri.Size = new System.Drawing.Size(397, 417);
+            this.GrpBoxUrunBilgileri.TabIndex = 8;
+            this.GrpBoxUrunBilgileri.TabStop = false;
+            this.GrpBoxUrunBilgileri.Text = "Ürün Bilgileri";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(45, 34);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(91, 15);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Ürün Modeli:";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(139, 27);
+            this.textBox4.MaxLength = 30;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(252, 25);
+            this.textBox4.TabIndex = 3;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(139, 58);
+            this.textBox5.MaxLength = 30;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(252, 25);
+            this.textBox5.TabIndex = 5;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 58);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 26);
+            this.comboBox1.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(38, 92);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(98, 15);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Arıza Tanımı:";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(139, 89);
+            this.textBox6.MaxLength = 30;
+            this.textBox6.Multiline = true;
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(252, 110);
+            this.textBox6.TabIndex = 8;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(45, 208);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(91, 15);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Ürün Durumu:";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(139, 205);
+            this.textBox7.MaxLength = 30;
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(252, 25);
+            this.textBox7.TabIndex = 10;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(31, 243);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(105, 15);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Ürün Takip No:";
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(139, 236);
+            this.textBox8.MaxLength = 30;
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(252, 25);
+            this.textBox8.TabIndex = 12;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label8.Location = new System.Drawing.Point(17, 270);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(119, 15);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "Müşteri Qr Kodu:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(139, 267);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(252, 90);
+            this.pictureBox1.TabIndex = 14;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label9.Location = new System.Drawing.Point(87, 370);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(49, 15);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "Ücret:";
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(139, 363);
+            this.textBox9.MaxLength = 30;
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(146, 25);
+            this.textBox9.TabIndex = 16;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(840, 129);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(972, 461);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.GrpBoxUrunBilgileri);
+            this.Controls.Add(this.GrpBoxEkBilgiler);
+            this.Controls.Add(this.GrpBoxAksesuarlar);
+            this.Controls.Add(this.GrpBoxMusteriBilgileri);
+            this.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.GrpBoxMusteriBilgileri.ResumeLayout(false);
+            this.GrpBoxMusteriBilgileri.PerformLayout();
+            this.GrpBoxAksesuarlar.ResumeLayout(false);
+            this.GrpBoxAksesuarlar.PerformLayout();
+            this.GrpBoxEkBilgiler.ResumeLayout(false);
+            this.GrpBoxEkBilgiler.PerformLayout();
+            this.GrpBoxUrunBilgileri.ResumeLayout(false);
+            this.GrpBoxUrunBilgileri.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox GrpBoxMusteriBilgileri;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.GroupBox GrpBoxAksesuarlar;
+        private System.Windows.Forms.TextBox TxtAksesuarlar;
+        private System.Windows.Forms.GroupBox GrpBoxEkBilgiler;
+        private System.Windows.Forms.TextBox TxtEkBilgiler;
+        private System.Windows.Forms.GroupBox GrpBoxUrunBilgileri;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button1;
+    }
+}
