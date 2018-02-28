78cacab84b8f97a7e5a3673e75789ab223cdace3
BySbF <36564237+BySbF@users.noreply.github.com>
2018-02-28 18:42:22 +0300

    Kod Bölüm - 2

--- RabtBilMusteriKayit/RabtBilMusteriKayit/FrmTeknikServisFormu.cs
+++ RabtBilMusteriKayit/RabtBilMusteriKayit/FrmTeknikServisFormu.cs
@@ -112,24 +112,26 @@ namespace RabtBilMusteriKayit
         {
             try
             {
-                MySqlCommand kaydet = new MySqlCommand("INSERT INTO musteribilgileri (MusteriAdi,FormNo,Telefon,UrunModeli,UrunSeriveImeiKodlari,UrunKodlari,ArizaTanimi,UrunDurumu,UrunTakipNo,Ucret,Aksesuarlar,EkBilgiler,Tarih) VALUES(@MusteriAdi,@FormNo,@Telefon,@UrunModeli,@UrunSeriveImeiKodlari,@UrunKodlari,@ArizaTanimi,@UrunDurumu,@UrunTakipNo,@Ucret,@Aksesuarlar,@EkBilgiler,@Tarih)", _baglanti);
-                kaydet.Parameters.AddWithValue("@MusteriAdi", TxtMusteriAdi.Text);
-                kaydet.Parameters.AddWithValue("@FormNo", TxtFormNo.Text);
-                kaydet.Parameters.AddWithValue("@Telefon", TxtTelefon.Text);
-                kaydet.Parameters.AddWithValue("@UrunModeli", TxtUrunModeli.Text);
-                kaydet.Parameters.AddWithValue("@UrunSeriveImeiKodlari", CmbBoxUrunKodlari.Text);
-                kaydet.Parameters.AddWithValue("@UrunKodlari", TxtUrunKodlari.Text);
-                kaydet.Parameters.AddWithValue("@ArizaTanimi", TxtArizaTanimi.Text);
-                kaydet.Parameters.AddWithValue("@UrunDurumu", TxtUrunDurumu.Text);
-                kaydet.Parameters.AddWithValue("@UrunTakipNo", Convert.ToInt32(TxtUrunTakipNo.Text));
+                MySqlCommand guncelle = new MySqlCommand("UPDATE musteribilgileri SET MusteriAdi=@MusteriAdi,FormNo=@FormNo,Telefon=@Telefon,UrunModeli=@UrunModeli,UrunSeriveImeiKodlari=@UrunSeriveImeiKodlari,UrunKodlari=@UrunKodlari,ArizaTanimi=@ArizaTanimi,UrunDurumu=@UrunDurumu,UrunTakipNo=@UrunTakipNo,Ucret=@Ucret,Aksesuarlar=@Aksesuarlar,EkBilgiler=@EkBilgiler,Tarih=@Tarih WHERE Id=@Id", _baglanti);
+                guncelle.Parameters.AddWithValue("@MusteriAdi", TxtMusteriAdi.Text);
+                guncelle.Parameters.AddWithValue("@FormNo", TxtFormNo.Text);
+                guncelle.Parameters.AddWithValue("@Telefon", TxtTelefon.Text);
+                guncelle.Parameters.AddWithValue("@UrunModeli", TxtUrunModeli.Text);
+                guncelle.Parameters.AddWithValue("@UrunSeriveImeiKodlari", CmbBoxUrunKodlari.Text);
+                guncelle.Parameters.AddWithValue("@UrunKodlari", TxtUrunKodlari.Text);
+                guncelle.Parameters.AddWithValue("@ArizaTanimi", TxtArizaTanimi.Text);
+                guncelle.Parameters.AddWithValue("@UrunDurumu", TxtUrunDurumu.Text);
+                guncelle.Parameters.AddWithValue("@UrunTakipNo", Convert.ToInt32(TxtUrunTakipNo.Text));
                 //kaydet.Parameters.AddWithValue("@URLKodu", TxtUrunTakipNo.Text);
-                kaydet.Parameters.AddWithValue("@Ucret", Convert.ToInt32(TxtUcret.Text));
-                kaydet.Parameters.AddWithValue("@Aksesuarlar", TxtAksesuarlar.Text);
-                kaydet.Parameters.AddWithValue("@EkBilgiler", TxtEkBilgiler.Text);
-                kaydet.Parameters.AddWithValue("@Tarih", DateTime.Parse(System.DateTime.Now.ToString()));
+                guncelle.Parameters.AddWithValue("@Ucret", Convert.ToInt32(TxtUcret.Text));
+                guncelle.Parameters.AddWithValue("@Aksesuarlar", TxtAksesuarlar.Text);
+                guncelle.Parameters.AddWithValue("@EkBilgiler", TxtEkBilgiler.Text);
+                guncelle.Parameters.AddWithValue("@Tarih", TlStripTarihSaat.Text);
+                guncelle.Parameters.AddWithValue("@Id", 7);
+                //guncelle.Parameters.AddWithValue("@Id", Convert.ToInt32(LblMusteriNo.Text));
                 _baglanti.Open();
-                kaydet.ExecuteNonQuery();
-                MessageBox.Show("Oluşturduğunuz Kayıt Kaydedilmiştir.");
+                guncelle.ExecuteNonQuery();
+                MessageBox.Show("Kaydınız Güncellenmiştir.");
                 _baglanti.Close();
                 _temizle();
             }
