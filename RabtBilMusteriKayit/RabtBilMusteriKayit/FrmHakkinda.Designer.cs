65a3da8be53c330af321dcb4b5a7c967b10274bb
BySbF <36564237+BySbF@users.noreply.github.com>
2018-03-14 11:53:56 +0300

    Hakkında Tasarım - 3

--- RabtBilMusteriKayit/RabtBilMusteriKayit/FrmHakkinda.Designer.cs
+++ RabtBilMusteriKayit/RabtBilMusteriKayit/FrmHakkinda.Designer.cs
@@ -35,43 +35,44 @@
             this.LinkLblLisansiDegistir = new System.Windows.Forms.LinkLabel();
             this.LblVersiyon = new System.Windows.Forms.Label();
             this.LblVersiyonNo = new System.Windows.Forms.Label();
-            this.PcTrBoxLogo = new System.Windows.Forms.PictureBox();
             this.GrpBoxSMFSoftWare = new System.Windows.Forms.GroupBox();
             this.LblCopyright = new System.Windows.Forms.Label();
             this.Lbl16300031011 = new System.Windows.Forms.Label();
             this.Lbl16300031031 = new System.Windows.Forms.Label();
             this.Lbl163000310 = new System.Windows.Forms.Label();
             this.LblLisansDurumu2 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.PcTrBoxLogo)).BeginInit();
+            this.PcTrBoxLogo = new System.Windows.Forms.PictureBox();
             this.GrpBoxSMFSoftWare.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PcTrBoxLogo)).BeginInit();
             this.SuspendLayout();
             // 
             // LblRabtBilMusteriKayitProgrami
             // 
             this.LblRabtBilMusteriKayitProgrami.AutoSize = true;
-            this.LblRabtBilMusteriKayitProgrami.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.LblRabtBilMusteriKayitProgrami.Location = new System.Drawing.Point(12, 22);
+            this.LblRabtBilMusteriKayitProgrami.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.LblRabtBilMusteriKayitProgrami.Location = new System.Drawing.Point(10, 18);
             this.LblRabtBilMusteriKayitProgrami.Name = "LblRabtBilMusteriKayitProgrami";
-            this.LblRabtBilMusteriKayitProgrami.Size = new System.Drawing.Size(256, 18);
+            this.LblRabtBilMusteriKayitProgrami.Size = new System.Drawing.Size(224, 15);
             this.LblRabtBilMusteriKayitProgrami.TabIndex = 0;
             this.LblRabtBilMusteriKayitProgrami.Text = "Rabt Bil Müşteri Kayıt Programı";
             // 
             // LblAnaSayfa
             // 
             this.LblAnaSayfa.AutoSize = true;
-            this.LblAnaSayfa.Location = new System.Drawing.Point(16, 124);
+            this.LblAnaSayfa.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.LblAnaSayfa.Location = new System.Drawing.Point(14, 72);
             this.LblAnaSayfa.Name = "LblAnaSayfa";
-            this.LblAnaSayfa.Size = new System.Drawing.Size(80, 18);
+            this.LblAnaSayfa.Size = new System.Drawing.Size(70, 15);
             this.LblAnaSayfa.TabIndex = 1;
             this.LblAnaSayfa.Text = "AnaSayfa:";
             // 
             // LinkLblAnaSayfaUrl
             // 
             this.LinkLblAnaSayfaUrl.AutoSize = true;
-            this.LinkLblAnaSayfaUrl.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.LinkLblAnaSayfaUrl.Location = new System.Drawing.Point(102, 124);
+            this.LinkLblAnaSayfaUrl.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.LinkLblAnaSayfaUrl.Location = new System.Drawing.Point(89, 72);
             this.LinkLblAnaSayfaUrl.Name = "LinkLblAnaSayfaUrl";
-            this.LinkLblAnaSayfaUrl.Size = new System.Drawing.Size(200, 18);
+            this.LinkLblAnaSayfaUrl.Size = new System.Drawing.Size(175, 15);
             this.LinkLblAnaSayfaUrl.TabIndex = 2;
             this.LinkLblAnaSayfaUrl.TabStop = true;
             this.LinkLblAnaSayfaUrl.Text = "http://www.casper.com.tr";
@@ -79,18 +80,20 @@
             // LblLisansDurumu
             // 
             this.LblLisansDurumu.AutoSize = true;
-            this.LblLisansDurumu.Location = new System.Drawing.Point(16, 175);
+            this.LblLisansDurumu.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.LblLisansDurumu.Location = new System.Drawing.Point(14, 106);
             this.LblLisansDurumu.Name = "LblLisansDurumu";
-            this.LblLisansDurumu.Size = new System.Drawing.Size(120, 18);
+            this.LblLisansDurumu.Size = new System.Drawing.Size(105, 15);
             this.LblLisansDurumu.TabIndex = 3;
             this.LblLisansDurumu.Text = "Lisans Durumu:";
             // 
             // LinkLblLisansiDegistir
             // 
             this.LinkLblLisansiDegistir.AutoSize = true;
-            this.LinkLblLisansiDegistir.Location = new System.Drawing.Point(262, 175);
+            this.LinkLblLisansiDegistir.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.LinkLblLisansiDegistir.Location = new System.Drawing.Point(229, 106);
             this.LinkLblLisansiDegistir.Name = "LinkLblLisansiDegistir";
-            this.LinkLblLisansiDegistir.Size = new System.Drawing.Size(136, 18);
+            this.LinkLblLisansiDegistir.Size = new System.Drawing.Size(119, 15);
             this.LinkLblLisansiDegistir.TabIndex = 4;
             this.LinkLblLisansiDegistir.TabStop = true;
             this.LinkLblLisansiDegistir.Text = "Lisansı Değiştir";
@@ -98,38 +101,34 @@
             // LblVersiyon
             // 
             this.LblVersiyon.AutoSize = true;
-            this.LblVersiyon.Location = new System.Drawing.Point(16, 73);
+            this.LblVersiyon.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.LblVersiyon.Location = new System.Drawing.Point(14, 45);
             this.LblVersiyon.Name = "LblVersiyon";
-            this.LblVersiyon.Size = new System.Drawing.Size(80, 18);
+            this.LblVersiyon.Size = new System.Drawing.Size(70, 15);
             this.LblVersiyon.TabIndex = 5;
             this.LblVersiyon.Text = "Versiyon:";
             // 
             // LblVersiyonNo
             // 
             this.LblVersiyonNo.AutoSize = true;
-            this.LblVersiyonNo.Location = new System.Drawing.Point(102, 73);
+            this.LblVersiyonNo.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.LblVersiyonNo.Location = new System.Drawing.Point(89, 45);
             this.LblVersiyonNo.Name = "LblVersiyonNo";
-            this.LblVersiyonNo.Size = new System.Drawing.Size(32, 18);
+            this.LblVersiyonNo.Size = new System.Drawing.Size(28, 15);
             this.LblVersiyonNo.TabIndex = 6;
             this.LblVersiyonNo.Text = "1.0";
-            // 
-            // PcTrBoxLogo
-            // 
-            this.PcTrBoxLogo.Image = global::RabtBilMusteriKayit.Properties.Resources.RabtBilLogo;
-            this.PcTrBoxLogo.Location = new System.Drawing.Point(404, 12);
-            this.PcTrBoxLogo.Name = "PcTrBoxLogo";
-            this.PcTrBoxLogo.Size = new System.Drawing.Size(386, 227);
-            this.PcTrBoxLogo.TabIndex = 7;
-            this.PcTrBoxLogo.TabStop = false;
             // 
             // GrpBoxSMFSoftWare
             // 
             this.GrpBoxSMFSoftWare.Controls.Add(this.Lbl163000310);
             this.GrpBoxSMFSoftWare.Controls.Add(this.Lbl16300031031);
             this.GrpBoxSMFSoftWare.Controls.Add(this.Lbl16300031011);
-            this.GrpBoxSMFSoftWare.Location = new System.Drawing.Point(19, 249);
+            this.GrpBoxSMFSoftWare.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.GrpBoxSMFSoftWare.Location = new System.Drawing.Point(17, 134);
+            this.GrpBoxSMFSoftWare.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.GrpBoxSMFSoftWare.Name = "GrpBoxSMFSoftWare";
-            this.GrpBoxSMFSoftWare.Size = new System.Drawing.Size(360, 108);
+            this.GrpBoxSMFSoftWare.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.GrpBoxSMFSoftWare.Size = new System.Drawing.Size(315, 90);
             this.GrpBoxSMFSoftWare.TabIndex = 8;
             this.GrpBoxSMFSoftWare.TabStop = false;
             this.GrpBoxSMFSoftWare.Text = "SMF-SoftWare";
@@ -137,58 +136,70 @@
             // LblCopyright
             // 
             this.LblCopyright.AutoSize = true;
-            this.LblCopyright.Location = new System.Drawing.Point(385, 288);
+            this.LblCopyright.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.LblCopyright.Location = new System.Drawing.Point(337, 177);
             this.LblCopyright.Name = "LblCopyright";
-            this.LblCopyright.Size = new System.Drawing.Size(408, 18);
+            this.LblCopyright.Size = new System.Drawing.Size(357, 15);
             this.LblCopyright.TabIndex = 9;
             this.LblCopyright.Text = "Copyright © 2018 SMF-SoftWare Tüm Hakları Saklıdır";
             // 
             // Lbl16300031011
             // 
             this.Lbl16300031011.AutoSize = true;
-            this.Lbl16300031011.Location = new System.Drawing.Point(6, 78);
+            this.Lbl16300031011.Location = new System.Drawing.Point(5, 65);
             this.Lbl16300031011.Name = "Lbl16300031011";
-            this.Lbl16300031011.Size = new System.Drawing.Size(256, 18);
+            this.Lbl16300031011.Size = new System.Drawing.Size(224, 15);
             this.Lbl16300031011.TabIndex = 10;
             this.Lbl16300031011.Text = "16300031011 - Selim Baki FINDIK";
             // 
             // Lbl16300031031
             // 
             this.Lbl16300031031.AutoSize = true;
-            this.Lbl16300031031.Location = new System.Drawing.Point(6, 21);
+            this.Lbl16300031031.Location = new System.Drawing.Point(5, 17);
             this.Lbl16300031031.Name = "Lbl16300031031";
-            this.Lbl16300031031.Size = new System.Drawing.Size(240, 18);
+            this.Lbl16300031031.Size = new System.Drawing.Size(210, 15);
             this.Lbl16300031031.TabIndex = 11;
             this.Lbl16300031031.Text = "16300031031 - Ali KÖMESÖĞÜTLÜ";
             // 
             // Lbl163000310
             // 
             this.Lbl163000310.AutoSize = true;
-            this.Lbl163000310.Location = new System.Drawing.Point(6, 51);
+            this.Lbl163000310.Location = new System.Drawing.Point(5, 42);
             this.Lbl163000310.Name = "Lbl163000310";
-            this.Lbl163000310.Size = new System.Drawing.Size(224, 18);
+            this.Lbl163000310.Size = new System.Drawing.Size(196, 15);
             this.Lbl163000310.TabIndex = 12;
             this.Lbl163000310.Text = "163000310?? - Fırat GÖRGÜLÜ";
             // 
             // LblLisansDurumu2
             // 
             this.LblLisansDurumu2.AutoSize = true;
+            this.LblLisansDurumu2.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.LblLisansDurumu2.ForeColor = System.Drawing.Color.Green;
-            this.LblLisansDurumu2.Location = new System.Drawing.Point(163, 175);
+            this.LblLisansDurumu2.Location = new System.Drawing.Point(143, 106);
             this.LblLisansDurumu2.Name = "LblLisansDurumu2";
-            this.LblLisansDurumu2.Size = new System.Drawing.Size(64, 18);
+            this.LblLisansDurumu2.Size = new System.Drawing.Size(56, 15);
             this.LblLisansDurumu2.TabIndex = 10;
             this.LblLisansDurumu2.Text = "Lisanlı";
             // 
+            // PcTrBoxLogo
+            // 
+            this.PcTrBoxLogo.Image = global::RabtBilMusteriKayit.Properties.Resources.RabtBilLogo;
+            this.PcTrBoxLogo.Location = new System.Drawing.Point(362, 10);
+            this.PcTrBoxLogo.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.PcTrBoxLogo.Name = "PcTrBoxLogo";
+            this.PcTrBoxLogo.Size = new System.Drawing.Size(310, 130);
+            this.PcTrBoxLogo.TabIndex = 11;
+            this.PcTrBoxLogo.TabStop = false;
+            // 
             // FrmHakkinda
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 18F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(804, 366);
+            this.ClientSize = new System.Drawing.Size(704, 237);
+            this.Controls.Add(this.PcTrBoxLogo);
             this.Controls.Add(this.LblLisansDurumu2);
             this.Controls.Add(this.LblCopyright);
             this.Controls.Add(this.GrpBoxSMFSoftWare);
-            this.Controls.Add(this.PcTrBoxLogo);
             this.Controls.Add(this.LblVersiyonNo);
             this.Controls.Add(this.LblVersiyon);
             this.Controls.Add(this.LinkLblLisansiDegistir);
@@ -196,18 +207,16 @@
             this.Controls.Add(this.LinkLblAnaSayfaUrl);
             this.Controls.Add(this.LblAnaSayfa);
             this.Controls.Add(this.LblRabtBilMusteriKayitProgrami);
-            this.Font = new System.Drawing.Font("Consolas", 11.25F);
-            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.MaximizeBox = false;
-            this.MaximumSize = new System.Drawing.Size(820, 405);
             this.MinimizeBox = false;
-            this.MinimumSize = new System.Drawing.Size(820, 405);
             this.Name = "FrmHakkinda";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Hakkında";
-            ((System.ComponentModel.ISupportInitialize)(this.PcTrBoxLogo)).EndInit();
             this.GrpBoxSMFSoftWare.ResumeLayout(false);
             this.GrpBoxSMFSoftWare.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PcTrBoxLogo)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -222,12 +231,12 @@
         private System.Windows.Forms.LinkLabel LinkLblLisansiDegistir;
         private System.Windows.Forms.Label LblVersiyon;
         private System.Windows.Forms.Label LblVersiyonNo;
-        private System.Windows.Forms.PictureBox PcTrBoxLogo;
         private System.Windows.Forms.GroupBox GrpBoxSMFSoftWare;
         private System.Windows.Forms.Label Lbl163000310;
         private System.Windows.Forms.Label Lbl16300031031;
         private System.Windows.Forms.Label Lbl16300031011;
         private System.Windows.Forms.Label LblCopyright;
         private System.Windows.Forms.Label LblLisansDurumu2;
+        private System.Windows.Forms.PictureBox PcTrBoxLogo;
     }
 }
